ede66bc610c6ae8541371bbfc34294cbaad1c49c
Dipta04 <diptakarmakar38@gmail.com>
2022-09-29 08:31:18 +0600

    cart work

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,7 +4,31 @@ import "./Cart.css"
 const Cart = () => {
     return (
         <div>
-            
+            <div>
+            <h4>Dipta Karmakar</h4>
+            <p><small>Chittagong,Bangladesh</small></p>
+            <p>Weight:70kg</p>
+            <p>Height:5.6</p>
+            <p>Age:20</p>
+            </div>
+
+            <div>
+                <h5>Add a Break</h5>
+                <div>
+                    <button>10s</button>
+                    <button>20s</button>
+                    <button>30s</button>
+                    <button>40s</button>
+                </div>
+            </div>
+
+            <div>
+               <h5>Exercise Time</h5>
+               <input type=""/>
+               <h5>Break Time</h5>
+               <input type="" />
+            </div>
+
         </div>
     );
 };
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSwimmer } from '@fortawesome/free-solid-svg-icons'
+import { faWalking } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import "./Header.css"
 
 const Header = () => {
     return (
-        <div className='header'>
-            <FontAwesomeIcon icon={faSwimmer}></FontAwesomeIcon>
-            <h1>Ultra-Activities-Active-Club</h1>   
+        <div>
+            <div className='header'>
+            <FontAwesomeIcon icon={faWalking}></FontAwesomeIcon>
+            <h1>Ultra-Activities-Active-Club</h1> 
+            </div>
+            <div>
+               <h3>Select Today's Exercise.</h3>
+            </div>
         </div>
     );
 };
